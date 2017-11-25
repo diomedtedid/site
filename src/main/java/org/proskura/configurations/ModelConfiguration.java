90af3bf9dea4efbf14ca3d2bb8a5e30844b05fe5
troy <vitalii.proskura@gmail.com>
2017-11-25 21:43:47 +0200

    added - db.properties and ModelConfiguration

---
+++ src/main/java/org/proskura/configurations/ModelConfiguration.java
@@ -0,0 +1,69 @@
+package org.proskura.configurations;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * Main Spring ORM configuration.
+ */
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"org.proskura.dao", "org.proskura.services"})
+@PropertySource("classpath:database.properties")
+@EnableJpaRepositories("org.proskura.dao")
+public class ModelConfiguration {
+
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+    @Value("${db.driver}")
+    private String driverClass;
+    @Value("${db.dialect}")
+    private String hibernateDialect;
+    @Value("classpath:db.sql")
+    private Resource  scriptResource;
+
+    @Bean(destroyMethod = "close")
+    public BasicDataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(true);
+        hibernateJpaVendorAdapter.setDatabasePlatform(hibernateDialect);
+
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        bean.setPackagesToScan("ua.goit.entity");
+        return bean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
